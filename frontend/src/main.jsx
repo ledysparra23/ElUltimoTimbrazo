404894fb513645dd1d24eb9bd34038f25147ea13
ledysparra23 <ledysparra23@gmail.com>
2026-04-13 01:08:49 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || '';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
